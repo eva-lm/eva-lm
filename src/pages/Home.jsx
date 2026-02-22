149c4f8645942e7f946c3e1387ae889e234c8a55
Eva <eva@MacBook-Pro-de-Eva.local>
2026-02-22 10:01:27 -0800

    home styles2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,36 @@
 import "../Styles/Layout/Home.scss";
 import { Row, Col } from "react-bootstrap";
 import imgHeader from "../img/img-header.svg";
+import Lottie from "lottie-react";
+import lineAnimation from "../animation/line.json";
+import { useRef, useEffect } from "react";
 
 const Home = () => {
+    const lottieRef = useRef(null);
+
+  useEffect(() => {
+    if (lottieRef.current) {
+      lottieRef.current.setSpeed(0.8);
+    }
+  }, []);
+
   return (
-           <div className="landing__bg-dibujo m-0 d-flex align-items-center justify-content-center">
+           <div className="landing__bg-dibujo m-0 d-flex flex-column align-items-center justify-content-center">
+            <div className="d-flex align-items-center justify-content-center">
         <Row className="intro">
-          <Col xs={12} className="text-end d-flex align-items-center">
-          <div className="m-1">
+          <Col xs={12} className="d-flex align-items-center">
+          <div className="text-end mt-4">
             <h1 className="fs-1 text-secondary">UX/UI Digital Product Designer</h1>
-            <h2 className="fs-6">
+            <div className="underline-animation w-100 d-flex align-items-center justify-content-center">
+                <Lottie
+                  lottieRef={lottieRef}
+                  animationData={lineAnimation}
+                  loop={2}
+                  autoplay={true}
+                  className="lottie-line"
+                />
+            </div>
+                                  <h2 className="fs-6">
               Hey there! I'm a creative graphic and web designer based in sunny
               San Francisco
             </h2>
@@ -19,6 +40,13 @@ const Home = () => {
           </div>
           </Col>
         </Row>
+            </div>
+              <div>
+                      {/* <h2 className="fs-6 mt-3">
+              Hey there! I'm a creative graphic and web designer based in sunny
+              San Francisco
+            </h2> */}
+        </div>
       </div>
   );
 };
